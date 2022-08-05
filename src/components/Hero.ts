ab63a8af1717621d9d455050a9656fd1b720b322
Rodrigo Queiroz <rodrigo.queiroz@dbccompany.com.br>
2022-08-05 01:46:30 -0300

    fix wrong doodle spacing on mobile

--- src/components/Hero.ts
+++ src/components/Hero.ts
@@ -8,7 +8,7 @@ const html = htm.bind(h)
 
 const Hero = () => {
   return html`
-    <div data-id="Hero" class="px-4 pb-[100px] md:pb-[250px] pt-[120px] flex flex-col md:flex-row md:justify-center items-center">
+    <div data-id="Hero" class="px-4 pb-[50px] md:pb-[250px] pt-[120px] flex flex-col md:flex-row md:justify-center items-center">
       <div class="w-full md:w-2/5">
         <h1 class="block antialiased text-5xl leading-tight mb-2 font-black tracking-normal text-[#1A237E]">Rodrigo Queiroz</h1>
         <p class="mb-4">Hi! I'm a Front End Engineer and this is a simple page to showcase my most relevant commercial projects. If you need more information or contact me check out my Linkedin profile in the button bellow</p>
@@ -20,7 +20,7 @@ const Hero = () => {
         <//>
       </div>
 
-      <div class="w-full md:w-2/5">
+      <div class="w-full md:w-2/5 mt-7 md:mt-0">
         <img class="max-w-[400px] mx-auto md:w-full md:max-w-[500]" src=${dancingDoodle} alt="" />
       </div>
     </div>
