036e302671a51a4c61a41b8307823e9959a8e147
RodrigoWebDev <rodrigo11397@protonmail.com>
2024-12-28 20:24:54 -0300

    feat: dynamic project filters

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -2,11 +2,70 @@ import enProjects from "../../data/en/projects.json";
 import ptProjects from "../../data/pt/projects.json";
 import Layout from "../../components/Layout";
 import { isEnglish } from "../../utils/locale";
+import { signal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
+
+const filters = signal<
+  {
+    name: string;
+    checked: boolean;
+  }[]
+>([]);
+
+const filteredProjects = signal<
+{
+  title: string;
+  link: string;
+  description: string;
+}[]
+>([])
 
 const Projects = (props: any) => {
-  console.log({ props })
-
   const projects = isEnglish() ? enProjects : ptProjects;
+
+  const onChange = (index: number) => {
+    const _filters = [...filters.value];
+    _filters[index].checked = !_filters[index].checked;
+
+    filters.value = _filters;
+  };
+
+  const getSkills = (str: string) => {
+    return str.replace("Skills: ", "").split(" · ")
+  }
+
+  const getFilters = () => {
+    let _filters: any[] = [];
+
+    projects.forEach((item) => {
+      _filters.push(getSkills(item.description));
+    });
+
+    _filters = [...new Set(_filters.flat())];
+
+    filters.value = _filters.map((item) => ({
+      name: item,
+      checked: false,
+    }));
+  };
+
+  useEffect(() => {
+    getFilters();
+  }, []);
+
+  useEffect(() => {
+    const selectedFilters = filters.value.filter((item) => item.checked).map((item) => item.name)
+
+    if (selectedFilters.length) {
+      filteredProjects.value = projects.filter((project) => {
+        return getSkills(project.description).some(item => selectedFilters.includes(item))
+      })
+    }else{
+      filteredProjects.value = [...projects]
+    }
+    console.log({ selectedFilters })
+    console.log({ filteredProjects })
+  }, [filters.value])
 
   return (
     <Layout>
@@ -14,19 +73,38 @@ const Projects = (props: any) => {
 
       <hr />
 
+      <br />
+      <details>
+        <summary>{isEnglish() ? "Filters" : "Filtros"}</summary>
+        <ul>
+          {filters.value.map((item, index) => {
+            return (
+              <li>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={item.checked}
+                    onChange={() => {
+                      onChange(index);
+                    }}
+                  />
+                  {item.name}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      </details>
+
       <ul>
-        {projects.map((project) => (
+        {filteredProjects.value.map((project) => (
           <li>
             <h3>
               <a href={project.link} target="_blank">
-              🔗 {project.title}
+                🔗 {project.title}
               </a>
             </h3>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: project.description,
-              }}
-            ></p>
+            <p>{project.description}</p>
           </li>
         ))}
       </ul>
